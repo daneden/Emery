6dc274c4863db6b467832cfab81753c03d66830a
Daniel Eden <dan.eden@me.com>
2021-03-06 10:27:01 +0000

    Initial Commit

---
+++ Emery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emery
+//
+//  Created by Daniel Eden on 06/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emery/EmeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmeryApp.swift
+//  Emery
+//
+//  Created by Daniel Eden on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
